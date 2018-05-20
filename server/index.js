254ff4768c33dac012907818faa3e738007c47c0
Raj <kmrrajj@gmail.com>
2018-05-20 22:48:36 +0530

    added router

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
 // App setup
 app.use(morgan('combined')); // used for logging.
 app.use(bodyParser.json({ type: '*/*'})); // parsing JSON requests
+router(app);
 
 
 
