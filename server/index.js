1e2015263e62472510dba7f13841ba3e7f2a11a5
Raj <kmrrajj@gmail.com>
2018-05-21 21:06:37 +0530

    added connection to mongodb

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB setup (connecting to local mongodb server)
+
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App setup
 app.use(morgan('combined')); // used for logging.
