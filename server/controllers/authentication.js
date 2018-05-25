3c2e8333edbf60742679eb96a8b44c7c40339948
Raj <kmrrajj@gmail.com>
2018-05-25 11:01:20 +0530

    added Authentication control

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,13 +1,33 @@
+const User = require('../models/user');
+
 exports.signup = function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
   // See if a user with email exists
-  
-  // If a user with email exists, return an Error
+  User.findOne({ email: email }, function (err, existingUser){
+    if (err) {
+      return next(err);
+    }
 
-  // If a user with email does not exist, create and save user record
+    // If a user with email exists, return an Error
+    if (existingUser) {
+      return res.status(422).send({error: 'Email already in use!'});
+    }
 
-  // Respond to request indicating the user was created
+    // If a user with email does not exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+    user.save(function(err) {
+      if(err) {
+        return next(err);
+      }
+
+      // Respond to request indicating the user was created
+      res.json(user);
+    })
+  });
 
 }
