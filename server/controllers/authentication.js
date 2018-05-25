d5972881d542cf1361ba4f61b01323b5392b24fd
Raj <kmrrajj@gmail.com>
2018-05-25 10:55:34 +0530

    added Controller setup

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,13 @@
 exports.signup = function (req, res, next) {
-  res.send({ success: 'true'});
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // See if a user with email exists
+  
+  // If a user with email exists, return an Error
+
+  // If a user with email does not exist, create and save user record
+
+  // Respond to request indicating the user was created
+
 }
