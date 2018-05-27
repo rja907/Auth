b3a46f69557623f049fe69f56a84fd52e387e417
Raj <kmrrajj@gmail.com>
2018-05-27 13:19:11 +0530

    added passport configuration

---
+++ server/services/passport.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Setup options for JwtStrategy
+const jwtOptions = {};
+
+// Create JwtStrategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // See if the userid in the payload exists in our DB
+  User.findById(payload.sub, function(err, user){
+    // If it does, call 'done' with that
+    if(err) {return done(err, false);}
+    // Otherwise, call done without that user object
+    if(user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+
+// Tell passport to use this Strategy
